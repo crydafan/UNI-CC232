b665121fc7600048f4f89a0e0fb9d43726e780e4
Leonardo <leonardo.estacio.h@gmail.com>
2026-04-01 22:48:34 -0500

    refactor(assignment_1): use pre-increment for loops

--- assignments/assignment_1/src/two_sum.cc
+++ assignments/assignment_1/src/two_sum.cc
@@ -6,7 +6,7 @@
 std::vector<int> two_sum(const std::vector<int> &nums, int target) {
   std::unordered_map<int, int> indices;
 
-  for (int i = 0; i < nums.size(); i++) {
+  for (int i = 0; i < nums.size(); ++i) {
     int remainder = target - nums[i];
 
     if (indices.find(remainder) != indices.end()) {
@@ -20,8 +20,8 @@ std::vector<int> two_sum(const std::vector<int> &nums, int target) {
 }
 
 std::vector<int> two_sum_naive(const std::vector<int> &nums, int target) {
-  for (int i = 0; i < nums.size(); i++) {
-    for (int j = 0; j < nums.size(); j++) {
+  for (int i = 0; i < nums.size(); ++i) {
+    for (int j = 0; j < nums.size(); ++j) {
       if (nums[i] + nums[j] == target)
         return {i, j};
     }
