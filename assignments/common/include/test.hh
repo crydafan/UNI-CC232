89e22712f73a6e5be1968786770adc702757a3ca
Leonardo <leonardo.estacio.h@gmail.com>
2026-04-03 20:32:01 -0500

    test(assignments): align title, description and status

--- assignments/common/include/test.hh
+++ assignments/common/include/test.hh
@@ -1,20 +1,26 @@
-// test.hh - minimal testing suite, not safe for multi-threading
 #pragma once
 
 #include <functional>
+#include <iomanip>
 #include <iostream>
 
 static int passing;
 static int failing;
 static int total;
 
+static constexpr size_t TEST_NAME_WIDTH = 28;
+static constexpr size_t TEST_DESC_WIDTH = 56;
+
 static void test(const std::string name, const std::string description,
                  std::function<bool()> func) {
   if (func()) {
-    std::cout << name << "\t\t" << description << "\t\t" << "Ok" << std::endl;
+    std::cout << std::left << std::setw(TEST_NAME_WIDTH) << name
+              << std::setw(TEST_DESC_WIDTH) << description << "Ok" << std::endl;
     ++passing;
   } else {
-    std::cerr << name << "\t\t" << description << "\t\t" << "Err" << std::endl;
+    std::cerr << std::left << std::setw(TEST_NAME_WIDTH) << name
+              << std::setw(TEST_DESC_WIDTH) << description << "Err"
+              << std::endl;
     ++failing;
   }
   ++total;
